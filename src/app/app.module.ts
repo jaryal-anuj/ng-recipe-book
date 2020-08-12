f030a1ca9b2044ad76e05899091eefea2720b689
anuj <anuj@ourdesignz.in>
2020-08-12 17:05:56 +0530

    adding auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +16,9 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,9 @@ import { RecipeService } from './recipes/recipe.service';
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +44,7 @@ import { RecipeService } from './recipes/recipe.service';
     AppRoutingModule,
 
   ],
-  providers: [ShoppingListService, RecipeService],
+  providers: [ShoppingListService, RecipeService, {provide:HTTP_INTERCEPTORS, useClass:AuthInterceptorService, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.modal';
-import { map, tap} from 'rxjs/operators';
+import { map, tap, take, exhaustMap} from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+
 
 @Injectable({
     providedIn:'root'
 })
 export class DataStorageService{
 
-    constructor(private http:HttpClient, private recipeService:RecipeService){
+    
+
+    constructor(
+        private http:HttpClient, 
+        private recipeService:RecipeService,
+        private authService:AuthService){
 
     }
 
@@ -21,18 +28,20 @@ export class DataStorageService{
     }
 
     fetchRecipes(){
-       return  this.http.get<Recipe[]>('https://ng-course-recipe-book-ae979.firebaseio.com/recipes.json')
-        .pipe(
-            map(recipes=>{
-                return recipes.map(recipe=>{
-                    return {...recipe, ingredients:recipe.ingredients?recipe.ingredients:[]};
+      
+            return  this.http.get<Recipe[]>('https://ng-course-recipe-book-ae979.firebaseio.com/recipes.json')
+                .pipe(
+                map(recipes=>{
+                    return recipes.map(recipe=>{
+                        return {...recipe, ingredients:recipe.ingredients?recipe.ingredients:[]};
+                    })
+                }),
+                tap(recipes=>{
+                    console.log(recipes)
+                    this.recipeService.setRecipes(recipes);
                 })
-            }),
-            tap(recipes=>{
-                console.log(recipes)
-                this.recipeService.setRecipes(recipes);
-            })
+            );
 
-        )
+
     }
 }
